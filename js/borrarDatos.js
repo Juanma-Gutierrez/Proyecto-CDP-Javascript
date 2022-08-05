a18e77a0d9310fc1f580ade99cf7a363a61f73b6
Juan Manuel Gutierrez <juanmanuelgutierrezmellado@gmail.com>
2022-08-05 19:06:35 +0200

    Confirmación de borrado

--- js/borrarDatos.js
+++ js/borrarDatos.js
@@ -3,13 +3,30 @@
 /*
  * Elimina el registro seleccionado
  * @param {tipo(string id)}
-*/
+ */
 function borrarDatos(id) {
     requestDB = indexedDB.open(indexedDbName, indexedDbVersion);
     requestDB.onsuccess = function (event) {
         db = event.target.result;
-        usersObjectStore = db.transaction(indexedDbStorage, "readwrite").objectStore(indexedDbStorage);
-        usersObjectStore.delete(id);
+        usersObjectStore = db
+            .transaction(indexedDbStorage, "readwrite")
+            .objectStore(indexedDbStorage);
+        var respuesta = alerta(id);
+        if ((respuesta == 1)) {
+            usersObjectStore.delete(id);
+        } else {
+            alert("Se ha cancelado el borrado del registro");
+        }
     };
     leerDatos();
+}
+
+function alerta(id) {
+    var mensaje;
+    var opcion = confirm("¿Deseas eliminar el registro seleccionado?");
+    if (opcion == true) {
+        return 1;
+    } else {
+        return 0;
+    }
 }
